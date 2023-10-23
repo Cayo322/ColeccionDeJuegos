54abd94a070044b3d95c72f1f3e5342585174aab
Cayo Alberto Phocco Machacca <cayo.phocco@tecsup.edu.pe>
2023-10-23 12:09:23 -0700

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Cayo on 10/23/23.
+//  Copyright © 2023 cayo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
