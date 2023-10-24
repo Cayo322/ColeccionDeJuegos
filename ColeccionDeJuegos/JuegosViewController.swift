af0b7d358991023e4abe4806a0e931fd074ab05b
Cayo Alberto Phocco Machacca <cayo.phocco@tecsup.edu.pe>
2023-10-23 18:58:25 -0700

    segunda version

---
+++ ColeccionDeJuegos/JuegosViewController.swift
@@ -0,0 +1,43 @@
+//
+//  JuegosViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Cayo on 10/23/23.
+//  Copyright © 2023 cayo. All rights reserved.
+//
+
+import UIKit
+
+class JuegosViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    var imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var JuegoImageView: UIImageView!
+    @IBOutlet weak var tituloTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+    
+    @IBAction func fotosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func camaraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func agregarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let juego = Juego(context: context)
+        juego.titulo = tituloTextField.text
+        juego.imagen = JuegoImageView.image?.jpegData (compressionQuality: 0.50)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController (animated: true)
+    }
+    func imagePickerController(_ _picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey : Any]){
+        let imagenSeleccionada = info[.originalImage] as? UIImage
+        JuegoImageView.image = imagenSeleccionada
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+}
